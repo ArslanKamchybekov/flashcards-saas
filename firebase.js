eb0c0f5154c99da4c50bcf1e879e6db2c32f0e10
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-08-15 15:18:04 -0500

    updated

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAnmLgp5NHlvoe6HoHRj7pGoPS3V2Keb6k",
-  authDomain: "flashcards-saas-cba16.firebaseapp.com",
-  projectId: "flashcards-saas-cba16",
-  storageBucket: "flashcards-saas-cba16.appspot.com",
-  messagingSenderId: "213650339905",
-  appId: "1:213650339905:web:f8048c0bed0959abdd6ec0",
-  measurementId: "G-RQVTY6TK1H"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
